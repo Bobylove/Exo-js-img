1762eb9f585712e536d34937e12aac6bd40a575a
Fred <delon.frederic.pro.job@gmail.com>
2016-10-07 10:48:00 +0200

    Fred end commit

--- app.js
+++ app.js
@@ -3,23 +3,13 @@ var species = {
 	bear   : "bear.jpg",
 	fish   : "fish.jpg"
 };
-
 function main(){
 	$("button").click(function(){
-	var dataAnimal = $(this).data('animal');
-	var pet =  species[dataAnimal];
-	$("img").attr('src', "img/" + pet);
+		var dataAnimal = $(this).data('animal');
+		var pet =  species[dataAnimal];
+		$("img").attr('src', "img/" + pet);
 	});
 }
-
-	// 1. placez un listener sur le click des éléments <button>
-
-	// 2. dans le callback récupérez le data attribute animal
-	// 3. récupérerez dans l'objet species la valeur correspond à l'attribut récupéré
-	// 4. ajoutez élément img en assignant à son attibut src la valeur précédemment récupérée
-	// 5. inserez cet élément dans l'élément ayant l'id holder
-	// 
-	// Conseils : avancez étape par étape, console.log et debugger autant que nécessaires !
-	$(document).ready(function(){
-		main();
-	});
+$(document).ready(function(){
+	main();
+});
